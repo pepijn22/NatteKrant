e1724a8614debf8d0e6c9ea0e0fb460e5c979cfd
Niels Lam <mail@nielslam.nl>
2018-10-12 11:16:23 +0200

    Rare datumding weghalen

--- index.php
+++ index.php
@@ -18,22 +18,11 @@
 			<!-- begin::Body -->
 			<div class="m-grid__item m-grid__item--fluid  m-grid m-grid--ver-desktop m-grid--desktop 	m-container m-container--responsive m-container--xxl m-page__container m-body">
 				<div class="m-grid__item m-grid__item--fluid m-wrapper">
-          <!-- BEGIN: Subheader -->
+         			<!-- BEGIN: Subheader -->
 					<div class="m-subheader ">
 						<div class="d-flex align-items-center">
 							<div class="mr-auto">
 								<h3 class="m-subheader__title ">Dashboard</h3>
-							</div>
-							<div>
-								<span class="m-subheader__daterange" id="m_dashboard_daterangepicker">
-									<span class="m-subheader__daterange-label">
-										<span class="m-subheader__daterange-title"></span>
-										<span class="m-subheader__daterange-date m--font-brand"></span>
-									</span>
-									<a href="#" class="btn btn-sm btn-brand m-btn m-btn--icon m-btn--icon-only m-btn--custom m-btn--pill">
-										<i class="la la-angle-down"></i>
-									</a>
-								</span>
 							</div>
 						</div>
 					</div>
@@ -49,7 +38,7 @@
 							include("dashboard.php");
 							$header = 'dashboard';
           				}
-          ?>
+          			?>
       	</div>		
 	</div>
 
