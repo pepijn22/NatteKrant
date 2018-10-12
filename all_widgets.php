9efc456205235bc815ff38e42e673a625a05c47b
Niels Lam <mail@nielslam.nl>
2018-10-12 11:46:54 +0200

    fix de all_widgets pagina

--- all_widgets.php
+++ all_widgets.php
@@ -1,52 +1,4 @@
-<?php include_once "include/header.php" ?>
 
-
-<!-- end::Head -->
-
-<!-- begin::Body -->
-<body class="m-page--wide m-header--fixed m-header--fixed-mobile m-footer--push m-aside--offcanvas-default">
-
-    <!-- begin:: Page -->
-    <div class="m-grid m-grid--hor m-grid--root m-page">
-
-        <!-- begin::Header -->
-
-        <?php include_once "include/menu.php" ?>
-
-        <!-- end::Header -->
-
-        <!-- begin::Body -->
-        <div class="m-grid__item m-grid__item--fluid  m-grid m-grid--ver-desktop m-grid--desktop 	m-container m-container--responsive m-container--xxl m-page__container m-body">
-            <div class="m-grid__item m-grid__item--fluid m-wrapper">
-      <!-- BEGIN: Subheader -->
-                <div class="m-subheader ">
-                    <div class="d-flex align-items-center">
-                        <div class="mr-auto">
-                            <h3 class="m-subheader__title ">Dashboard</h3>
-                        </div>
-                        <div>
-                            <span class="m-subheader__daterange" id="m_dashboard_daterangepicker">
-                                <span class="m-subheader__daterange-label">
-                                    <span class="m-subheader__daterange-title"></span>
-                                    <span class="m-subheader__daterange-date m--font-brand"></span>
-                                </span>
-                                <a href="#" class="btn btn-sm btn-brand m-btn m-btn--icon m-btn--icon-only m-btn--custom m-btn--pill">
-                                    <i class="la la-angle-down"></i>
-                                </a>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-
-                <?php
-                    $header = 'Dashboard';
-                    if(isset($_GET['page'])){
-                        if($_GET['page'] == 'klanten'){
-                          include ("klanten.php");
-                          $header = 'Klanten';
-                        }
-                    } else {
-                ?>
 
                 <!-- END: Subheader -->
                 <div class="m-content">
@@ -2462,21 +2414,4 @@
                                 </div>
                             </div>
 
-                            <!--end:: Widgets/Sales States-->
-                        </div>
-                    </div>
-
-                    <!--End::Section-->
-                </div>
-      <?php
-      }
-      ?>
-      </div>
-
-
-            <!--</div>-->
-        </div>
-
-        <!-- end::Body -->
-
-        <?php include_once "include/footer.php" ?>
+                        
