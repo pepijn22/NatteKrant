c4b80cae59551bbe90cbf7da5a540b08067cd284
Stan Derix <s.derix@juist.nl>
2018-10-20 13:49:25 +0200

    table cleaner

--- orders.php
+++ orders.php
@@ -6,24 +6,6 @@
     <div class="m-portlet__body">
     		<!--begin: Datatable -->
     		<div id="m_table_1_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
-          <div class="row">
-            <div class="col-sm-12 col-md-6">
-              <div class="dataTables_length" id="m_table_1_length">
-                <label>Show <select name="m_table_1_length" aria-controls="m_table_1" class="custom-select custom-select-sm form-control form-control-sm">
-                  <option value="10">10</option>
-                  <option value="25">25</option>
-                  <option value="50">50</option>
-                  <option value="100">100</option>
-                </select> entries
-              </label>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6">
-            <div id="m_table_1_filter" class="dataTables_filter">
-              <label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="m_table_1"></label>
-            </div>
-          </div>
-        </div>
 
         <div class="row">
           <div class="col-sm-12">
@@ -92,24 +74,6 @@
     <div class="m-portlet__body">
     		<!--begin: Datatable -->
     		<div id="m_table_1_wrapper" class="dataTables_wrapper dt-bootstrap4 no-footer">
-          <div class="row">
-            <div class="col-sm-12 col-md-6">
-              <div class="dataTables_length" id="m_table_1_length">
-                <label>Show <select name="m_table_1_length" aria-controls="m_table_1" class="custom-select custom-select-sm form-control form-control-sm">
-                  <option value="10">10</option>
-                  <option value="25">25</option>
-                  <option value="50">50</option>
-                  <option value="100">100</option>
-                </select> entries
-              </label>
-            </div>
-          </div>
-          <div class="col-sm-12 col-md-6">
-            <div id="m_table_1_filter" class="dataTables_filter">
-              <label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="m_table_1"></label>
-            </div>
-          </div>
-        </div>
 
         <div class="row">
           <div class="col-sm-12">
