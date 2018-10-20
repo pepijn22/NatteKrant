2a8989ec135c06b866cb21f0e30c0ee6b01a3c02
Pepijn Schonenberg <>
2018-10-20 14:18:58 +0200

    orders, magazijn

--- order.php
+++ order.php
@@ -19,17 +19,22 @@ if(isset($_GET['orderid'])){
 			  			<tr role="row">
                       <th class="sorting" tabindex="0" aria-controls="m_table_1" rowspan="1" colspan="1" aria-label="Naam: activate to sort naam ascending">Product</th>
                       <th class="sorting" tabindex="0" aria-controls="m_table_1" rowspan="1" colspan="1" aria-label="">Aantal</th>
+                       <th class="sorting" tabindex="0" aria-controls="m_table_1" rowspan="1" colspan="1" aria-label="">Gang</th>
+                        <th class="sorting" tabindex="0" aria-controls="m_table_1" rowspan="1" colspan="1" aria-label="">Robot</th>
+                         <th class="sorting" tabindex="0" aria-controls="m_table_1" rowspan="1" colspan="1" aria-label="">Robot type</th>
                     </tr>
 						</thead>
 								<tbody>
                 <?php
 
-                    $sql = "Select * from klantorder_producten as kp
-
+                    $sql = "Select p.naam, kp.aantal, r.Gang_gang_id, r.type, r.robot_id from klantorder_producten as kp
                     left join product as p 
                     on kp.Product_product_id = p.product_id
-                    
-                    where Klantorder_order_id = ".$orderid;
+                    JOIN schap s ON s.schap_id = p.Schap_schap_id
+                    JOIN gang g ON g.gang_id = s.gang_gang_id
+                    join robot r on g.gang_id = r.gang_gang_id
+                    where Klantorder_order_id =
+                 ".$orderid;
 
                     foreach ($conn->query($sql) as $row) {
                         ?>
@@ -43,6 +48,15 @@ if(isset($_GET['orderid'])){
                             </td>
                             <td>
                                 <?=$row['aantal']?>
+                            </td>
+                            <td>
+                                <?=$row['Gang_gang_id']?>
+                            </td>
+                            <td>
+                                <?=$row['robot_id']?>
+                            </td>
+                            <td>
+                                <?=$row['type']?>
                             </td>
         				  									
                               
