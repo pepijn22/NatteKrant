42fe3a113ad8aa7a7544f65008318f6bc0b9730a
Stan Derix <s.derix@juist.nl>
2018-10-21 12:29:23 +0200

    Dashboard geupdatet

---
+++ dashboard.php
@@ -0,0 +1,305 @@
+<div class="m-content">
+
+						<!--Begin::Section-->
+						<div class="row">
+							<div class="col-xl-4">
+
+								<!--begin:: Widgets/Top Products-->
+								<div class="m-portlet m-portlet--bordered-semi m-portlet--full-height ">
+									<div class="m-portlet__head">
+										<div class="m-portlet__head-caption">
+											<div class="m-portlet__head-title">
+												<h3 class="m-portlet__head-text">
+													Populaire medicijnen
+												</h3>
+											</div>
+										</div>
+									</div>
+									<div class="m-portlet__body">
+
+										<!--begin::Widget5-->
+										<div class="m-widget4">
+											<div class="m-widget4__chart m-portlet-fit--sides m--margin-top-10 m--margin-top-20" style="height:260px;">
+												<canvas id="m_chart_trends_stats"></canvas>
+											</div>
+											<div class="m-widget4__item">
+												<div class="m-widget4__info">
+													<span class="m-widget4__title">
+														Naproxen
+													</span>
+												</div>
+												<span class="m-widget4__ext">
+													<span class="m-widget4__number m--font-success">€1700</span>
+												</span>
+											</div>
+											<div class="m-widget4__item">
+												<div class="m-widget4__info">
+													<span class="m-widget4__title">
+														Isradipine
+													</span>
+												</div>
+												<span class="m-widget4__ext">
+													<span class="m-widget4__number m--font-success">€670</span>
+												</span>
+											</div>
+											<div class="m-widget4__item">
+												<div class="m-widget4__info">
+													<span class="m-widget4__title">
+														Oxazepam
+													</span>
+												</div>
+												<span class="m-widget4__ext">
+													<span class="m-widget4__number m--font-success">€352</span>
+												</span>
+											</div>
+										</div>
+
+										<!--end::Widget 5-->
+									</div>
+								</div>
+
+								<!--end:: Widgets/Top Products-->
+							</div>
+							<div class="col-xl-4">
+
+								<!--begin:: Widgets/Activity-->
+								<div class="m-portlet m-portlet--bordered-semi m-portlet--widget-fit m-portlet--full-height m-portlet--skin-light  m-portlet--rounded-force">
+									<div class="m-portlet__head">
+										<div class="m-portlet__head-caption">
+											<div class="m-portlet__head-title">
+												<h3 class="m-portlet__head-text m--font-light">
+													Activiteit
+												</h3>
+											</div>
+										</div>
+									</div>
+									<div class="m-portlet__body">
+										<div class="m-widget17">
+											<div class="m-widget17__visual m-widget17__visual--chart m-portlet-fit--top m-portlet-fit--sides m--bg-danger">
+												<div class="m-widget17__chart" style="height:320px;">
+													<canvas id="m_chart_activities"></canvas>
+												</div>
+											</div>
+											<div class="m-widget17__stats">
+												<div class="m-widget17__items m-widget17__items-col1">
+													<div class="m-widget17__item">
+														<span class="m-widget17__icon">
+															<i class="flaticon-truck m--font-brand"></i>
+														</span>
+														<span class="m-widget17__subtitle">
+															Verstuurd
+														</span>
+														<span class="m-widget17__desc">
+															15 nieuwe berichten
+														</span>
+													</div>
+													<div class="m-widget17__item">
+														<span class="m-widget17__icon">
+															<i class="flaticon-paper-plane m--font-info"></i>
+														</span>
+														<span class="m-widget17__subtitle">
+															Retour gestuurd
+														</span>
+														<span class="m-widget17__desc">
+															27 nieuwe orders
+														</span>
+													</div>
+												</div>
+												<div class="m-widget17__items m-widget17__items-col2">
+													<div class="m-widget17__item">
+														<span class="m-widget17__icon">
+															<i class="flaticon-pie-chart m--font-success"></i>
+														</span>
+														<span class="m-widget17__subtitle">
+															Besteld
+														</span>
+														<span class="m-widget17__desc">
+															52 nieuwe orders
+														</span>
+													</div>
+													<div class="m-widget17__item">
+														<span class="m-widget17__icon">
+															<i class="flaticon-time m--font-danger"></i>
+														</span>
+														<span class="m-widget17__subtitle">
+															Wanbetalers
+														</span>
+														<span class="m-widget17__desc">
+															4 facturen te laat
+														</span>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<!--end:: Widgets/Activity-->
+							</div>
+							<div class="col-xl-4">
+
+								<!--begin:: Widgets/Blog-->
+								<div class="m-portlet m-portlet--bordered-semi m-portlet--full-height  m-portlet--rounded-force">
+									<div class="m-portlet__head m-portlet__head--fit">
+										<div class="m-portlet__head-caption">
+											<div class="m-portlet__head-action">
+												<button type="button" class="btn btn-sm m-btn--pill  btn-brand">Blog</button>
+											</div>
+										</div>
+									</div>
+									<div class="m-portlet__body">
+										<div class="m-widget19">
+											<div class="m-widget19__pic m-portlet-fit--top m-portlet-fit--sides" style="min-height-: 286px">
+												<img src="assets/app/media/img//blog/blog1.jpg" alt="">
+												<h3 class="m-widget19__title m--font-light">
+													Nieuwe functie TimaFlu Systeem komt binnenkort
+												</h3>
+												<div class="m-widget19__shadow"></div>
+											</div>
+											<div class="m-widget19__content">
+												<div class="m-widget19__header">
+													<div class="m-widget19__user-img">
+														<img class="m-widget19__img" src="assets/app/media/img//users/user1.jpg" alt="">
+													</div>
+													<div class="m-widget19__info">
+														<span class="m-widget19__username">
+															Anna Krox
+														</span>
+														<br>
+														<span class="m-widget19__time">
+															UX/UI Designer, Natte Krant ICT Solutions
+														</span>
+													</div>
+													<div class="m-widget19__stats">
+														<span class="m-widget19__number m--font-brand">
+															18
+														</span>
+														<span class="m-widget19__comment">
+															reacties
+														</span>
+													</div>
+												</div>
+												<div class="m-widget19__body">
+													Lorem Ipsum is simply dummy text of the printing and typesetting industry scrambled it to make text of the printing and typesetting industry scrambled a type specimen book text of the dummy text of the printing printing and typesetting industry scrambled
+													dummy text of the printing.
+												</div>
+											</div>
+											<div class="m-widget19__action">
+												<button type="button" class="btn m-btn--pill btn-secondary m-btn m-btn--hover-brand m-btn--custom">Lees meer</button>
+											</div>
+										</div>
+									</div>
+								</div>
+
+								<!--end:: Widgets/Blog-->
+							</div>
+						</div>
+
+						<!--End::Section-->
+
+						<!--Begin::Section-->
+						<div class="m-portlet">
+							<div class="m-portlet__body  m-portlet__body--no-padding">
+								<div class="row m-row--no-padding m-row--col-separator-xl">
+									<div class="col-xl-4">
+
+										<!--begin:: Widgets/Stats2-1 -->
+										<div class="m-widget1">
+											<div class="m-widget1__item">
+												<div class="row m-row--no-padding align-items-center">
+													<div class="col">
+														<h3 class="m-widget1__title">Week omzet</h3>
+														<span class="m-widget1__desc">Gemiddelde omzet per week</span>
+													</div>
+													<div class="col m--align-right">
+														<span class="m-widget1__number m--font-brand">+€17,800</span>
+													</div>
+												</div>
+											</div>
+											<div class="m-widget1__item">
+												<div class="row m-row--no-padding align-items-center">
+													<div class="col">
+														<h3 class="m-widget1__title">Orders</h3>
+														<span class="m-widget1__desc">Weekelijkse orders</span>
+													</div>
+													<div class="col m--align-right">
+														<span class="m-widget1__number m--font-danger">+18</span>
+													</div>
+												</div>
+											</div>
+											<div class="m-widget1__item">
+												<div class="row m-row--no-padding align-items-center">
+													<div class="col">
+														<h3 class="m-widget1__title">Reacties klanten</h3>
+														<span class="m-widget1__desc">Contact met klanter per week</span>
+													</div>
+													<div class="col m--align-right">
+														<span class="m-widget1__number m--font-success">-27,49%</span>
+													</div>
+												</div>
+											</div>
+										</div>
+
+										<!--end:: Widgets/Stats2-1 -->
+									</div>
+									<div class="col-xl-4">
+
+										<!--begin:: Widgets/Daily Sales-->
+										<div class="m-widget14">
+											<div class="m-widget14__header m--margin-bottom-30">
+												<h3 class="m-widget14__title">
+													Dagelijkse verkoop
+												</h3>
+												<span class="m-widget14__desc">
+													Bekijk iedere kolom voor meer details
+												</span>
+											</div>
+											<div class="m-widget14__chart" style="height:120px;">
+												<canvas id="m_chart_daily_sales"></canvas>
+											</div>
+										</div>
+
+										<!--end:: Widgets/Daily Sales-->
+									</div>
+									<div class="col-xl-4">
+
+										<!--begin:: Widgets/Profit Share-->
+										<div class="m-widget14">
+											<div class="m-widget14__header">
+												<h3 class="m-widget14__title">
+													Winst
+												</h3>
+											</div>
+											<div class="row  align-items-center">
+												<div class="col">
+													<div id="m_chart_profit_share" class="m-widget14__chart" style="height: 160px">
+														<div class="m-widget14__stat">45</div>
+													</div>
+												</div>
+												<div class="col">
+													<div class="m-widget14__legends">
+														<div class="m-widget14__legend">
+															<span class="m-widget14__legend-bullet m--bg-accent"></span>
+															<span class="m-widget14__legend-text">37% support klanten</span>
+														</div>
+														<div class="m-widget14__legend">
+															<span class="m-widget14__legend-bullet m--bg-warning"></span>
+															<span class="m-widget14__legend-text">47% export medicijnen</span>
+														</div>
+														<div class="m-widget14__legend">
+															<span class="m-widget14__legend-bullet m--bg-brand"></span>
+															<span class="m-widget14__legend-text">19% Overig</span>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+
+										<!--end:: Widgets/Profit Share-->
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!--End::Section-->
+					</div>
